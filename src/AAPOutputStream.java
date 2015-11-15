156129add324fd05386c324c895326ef94fbb645
Wendi Tang <wtang45@gatech.edu>
2015-11-15 13:57:10 -0500

    Edited

--- src/AAPOutputStream.java
+++ src/AAPOutputStream.java
@@ -16,6 +16,7 @@ public class AAPOutputStream {
 	private byte[] packetBuffer = new byte[AAPPacket.PACKET_SIZE];
 	private ArrayList<DatagramPacket> packetsList; 
 	private short currentWindowSize = 10;
+	private static short MAX_WINDOW_SIZE = 10;
 	
 	private String recverAddress;
 	private int recverPort;
@@ -29,6 +30,7 @@ public class AAPOutputStream {
 		this.recverAddress = recverAddress;
 		this.recverPort = recverPort;
 		this.packetsList = new ArrayList();
+		this.currentWindowSize = MAX_WINDOW_SIZE;
 		
 	}
 	
@@ -49,23 +51,35 @@ public class AAPOutputStream {
 	}
 	
 	private void send(byte[] b) throws FlagNotFoundException, IOException, PayLoadSizeTooLargeException{
+		putPacketInQueue(b);
+		
+		
+		
+	}
+	
+	private void putPacketInQueue(byte[] b) throws FlagNotFoundException, IOException, PayLoadSizeTooLargeException{
+		
 		int byteLeft = b.length;
 		int currentPos = 0;
-		/*  DatagramPacket sendPacket = new DatagramPacket(bytesToSend,  // Sending packet
-		            bytesToSend.length, serverAddress, servPort);*/
-		
 		while(byteLeft - AAPPacket.MAX_PAYLOAD_SIZE >0){
-				sendAAPacket = new AAPPacket(currentSeqNum, 0, AAPPacket.NULL_FLAG,
-						(short)0, Arrays.copyOfRange(b, currentPos, currentPos+AAPPacket.MAX_PAYLOAD_SIZE)); 
-				currentPos = currentPos+AAPPacket.MAX_PAYLOAD_SIZE;
-				packetsList.add(new DatagramPacket(sendAAPacket.getPacketData(),
-						sendAAPacket.getPacketData().length,InetAddress.getByName(recverAddress), recverPort));
+			sendAAPacket = new AAPPacket(currentSeqNum, 0, AAPPacket.NULL_FLAG,
+					(short)0, Arrays.copyOfRange(b, currentPos, currentPos+AAPPacket.MAX_PAYLOAD_SIZE)); 
+			currentPos = currentPos+AAPPacket.MAX_PAYLOAD_SIZE;
+			packetsList.add(new DatagramPacket(sendAAPacket.getPacketData(),
+					sendAAPacket.getPacketData().length,InetAddress.getByName(recverAddress), recverPort));
+			currentSeqNum = AAPUtils.incrementSeqNum(currentSeqNum);
 		}
 		sendAAPacket = new AAPPacket(currentSeqNum, 0, AAPPacket.NULL_FLAG,
 				(short)0, Arrays.copyOfRange(b, currentPos, currentPos+AAPPacket.MAX_PAYLOAD_SIZE));
 		packetsList.add(new DatagramPacket(sendAAPacket.getPacketData(),
 				sendAAPacket.getPacketData().length,InetAddress.getByName(recverAddress), recverPort));
+		currentSeqNum = AAPUtils.incrementSeqNum(currentSeqNum);
 	}
 	
+	private void sendPacketsInWindow() throws IOException{
+		for(int i = 0; i < Math.min(packetsList.size(),currentWindowSize); i++){
+			sendSocket.send(packetsList.get(i));
+		}
+	}
 	
 }
