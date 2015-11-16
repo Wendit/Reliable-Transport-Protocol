443fd882a319036e81ae64b276cb4c1033b6f206
ludky <ludky109@hotmail.com>
2015-11-16 00:47:06 -0500

    get started with FAA

---
+++ src/FAA_client.java
@@ -0,0 +1,35 @@
+/*
+ * 
+ * Command-line: FxA-client X A P 
+
+	The command-line arguments are: 
+	
+	X: the port number at which the FxA-client’s UDP socket should bind to (even number). Please remember that this port number should be equal to the server’s port number minus 1. 
+	
+	A: the IP address of NetEmu
+	P: the UDP port number of NetEmu 
+	
+	Command: connect - The FxA-client connects to the FxA-server (running at the same IP host). 
+	
+	Command: get F - The FxA-client downloads file F from the server (if F exists in the same directory with the FxA-server program). 
+	
+	Command: post F - The FxA-client uploads file F to the server (if F exists in the same directory with the FxA-client program). This feature will be treated as extra credit for up to 20 project points.
+	
+	Command: window W (only for projects that support configurable flow window) W: the maximum receiver’s window-size at the FxA-Client (in segments). 
+	
+	Command: disconnect - The FxA-client terminates gracefully from the FxA-server. 
+
+
+ * 
+ * */
+public class FAA_client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/FAA_server.java
@@ -0,0 +1,30 @@
+/*
+ * 
+ * FxA SERVER
+ * 
+ * Command-line: FxA-server X A P 
+
+	The command-line arguments are:
+	X: the port number at which the FxA-server’s UDP socket should bind to (odd number) 
+
+	A: the IP address of NetEmu
+	P: the UDP port number of NetEmu 
+ * 
+ * Command: window W (only for projects that support pipelined and bi- directional transfers) 
+
+	W: the maximum receiver’s window-size at the FxA-Server (in segments). 
+ * 
+ * Command: terminate Shut-down FxA-Server gracefully. 
+ * 
+ */
+public class FAA_server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
