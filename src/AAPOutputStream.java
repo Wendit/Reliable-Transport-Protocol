4ce3bb203da44fd22e0adad59eb8f71c0f0b96a9
Wendi Tang <wtang45@gatech.edu>
2015-11-20 17:13:37 -0500

    Almost finish outputstream

--- src/AAPOutputStream.java
+++ src/AAPOutputStream.java
@@ -8,6 +8,7 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class AAPOutputStream {
@@ -16,13 +17,14 @@ public class AAPOutputStream {
 	private DatagramPacket recvPacket;
 	private AAPPacket sendAAPacket;
 	private byte[] packetBuffer = new byte[AAPPacket.PACKET_SIZE];
-	private Deque<AAPPacket> packetsList; 
-	private Deque<AAPPacket> packetWindow;
+	private LinkedList<AAPPacket> packetsList; 
+	private LinkedList<AAPPacket> packetWindow;
 	private short currentWindowSize = 10;
 	private static short MAX_WINDOW_SIZE = 10;
 	
 	private String recverAddress;
 	private int recverPort;
+	private int lastAcked;
 	
 	private int currentSeqNum;
 	
@@ -36,19 +38,22 @@ public class AAPOutputStream {
 		this.packetWindow = new LinkedList();
 		this.currentWindowSize = MAX_WINDOW_SIZE;
 		this.recvPacket = new DatagramPacket(packetBuffer, AAPPacket.PACKET_SIZE);
+		this.lastAcked = -1;
 		
 	}
 	
-	public void write(byte b){
-		
+	public void write(byte b) throws FlagNotFoundException, IOException, PayLoadSizeTooLargeException{
+		byte[] temp = {b};
+		send(temp);
 	}
 	
-	public void write(byte[] bArray){
-		
+	public void write(byte[] bArray) throws FlagNotFoundException, IOException, PayLoadSizeTooLargeException{
+		send(bArray);
 	}
 	
-	public void write(byte[] b, int off, int len){
-		
+	public void write(byte[] b, int off, int len) throws FlagNotFoundException, IOException, PayLoadSizeTooLargeException{
+		byte[] temp = Arrays.copyOfRange(b, off, off+len);
+		send(temp);
 	}
 	
 	public void close(){
@@ -57,9 +62,19 @@ public class AAPOutputStream {
 	
 	private void send(byte[] b) throws FlagNotFoundException, IOException, PayLoadSizeTooLargeException{
 		putPacketInQueue(b);
-		
-		
-		
+		sendPackets();	
+	}
+	
+	private void sendPackets() throws IOException{	  
+	  fillWindow();//Automatically sends the packets added
+	  lastAcked = packetWindow.getFirst().getSeqNum();
+	  while(packetsList.size() != 0 || packetWindow.size() != 0){
+		  recvAckAndFillWindow();
+		  //if ackRecieve times out send all packets again if the window is not empty 
+		  if(packetWindow.size() !=0 && lastAcked == packetWindow.getFirst().getSeqNum())
+			  sendPacketInWindow(packetWindow.size());
+	  }
+	  //Then if a an ack if recieved, discad head and add new packet(if any)
 	}
 	
 	private void putPacketInQueue(byte[] b) throws FlagNotFoundException, IOException, PayLoadSizeTooLargeException{
@@ -79,21 +94,7 @@ public class AAPOutputStream {
 		currentSeqNum = AAPUtils.incrementSeqNum(currentSeqNum);
 	}
 	
-	private void sendPackets() throws IOException{
-	  putPacketsInWindow();
-	  //First send all packets in window
-	  for(int i =0; i < packetWindow.size();i++){
-		  sendSocket.send(new DatagramPacket(sendAAPacket.getPacketData(),
-				  AAPPacket.PACKET_SIZE,InetAddress.getByName(recverAddress), recverPort));
-	  }
-	  while(packetsList.size() != 0 && packetWindow.size() != 0){
-		  recvAckAndFillWindow();
-	  }
-	  //Then if a an ack if recieved, discad head and add new packet(if any)
-	}
-	
-	private int recvAckAndFillWindow() throws IOException{
-		int lastAcked = -1;
+	private void recvAckAndFillWindow() throws IOException{	
 		AAPPacket recvAAPPacket;
 		while(true){		
 			try {
@@ -109,11 +110,17 @@ public class AAPOutputStream {
 				 if(recvAAPPacket.getFlags() == AAPPacket.ACK_FLAG ||
 						 recvAAPPacket.getFlags() == AAPPacket.FIN_ACK_FLAG ||
 						 recvAAPPacket.getFlags() == AAPPacket.SYN_ACK_FLAG ){
-					 lastAcked = recvAAPPacket.getAckNum();
+					 lastAcked = Math.max(lastAcked,recvAAPPacket.getAckNum());
+					 //discard all acked packets
+					 discardAckedPackets(lastAcked);
 					 //Adjust window size according to remaining buffe size
 					 currentWindowSize = (short) Math.min(MAX_WINDOW_SIZE,(short)Math.floor
 							 (recvAAPPacket.getWindowSize()/AAPPacket.MAX_PAYLOAD_SIZE));
-					 adjustWindowSize(currentWindowSize);
+					 //Will only actually shirink if the number of packets in window is greater than
+					 //the window size
+					 shirinkWindowSize(currentWindowSize);
+					 //Fill the window, until list is empty or packets in window reach the maxium window size
+					 fillWindow();
 				 }
 			 }catch(FlagNotFoundException e){
 				 DebugUtils.debugPrint(e.getMessage());
@@ -121,32 +128,54 @@ public class AAPOutputStream {
 				 DebugUtils.debugPrint(e.getMessage());
 			 }
 		}
-		return lastAcked;
 	}
 	
-	private void adjustWindowSize(int newWindowSize) throws IOException{
+	private void shirinkWindowSize(int newWindowSize) throws IOException{
 		while(packetWindow.size()>newWindowSize){
 			moveWindowTailToList();
 		}
-		while(packetWindow.size() < newWindowSize && packetsList.size()!=0){
-			moveListHeadToWindow();
-		}
 	}
 	
-	
-	private void putPacketsInWindow() throws IOException{
-		while(packetWindow.size() < currentWindowSize && packetsList.size() != 0){
-			moveListHeadToWindow();
-		}
-	}
-	private void moveListHeadToWindow(){
+	private AAPPacket moveListHeadToWindow(){
+		AAPPacket polled;
 		if(packetWindow.size() < currentWindowSize){
-			packetWindow.add(packetsList.poll());
+			polled = packetsList.poll();
+			packetWindow.add(polled);
+			return polled;
 		}
+		return null;
 	}
 	
 	private void moveWindowTailToList(){
 			packetsList.push(packetWindow.pollLast());
+	}
+	
+	private void discardAckedPackets(int ackedNumber){
+		while(packetWindow.size()!=0 && packetWindow.peek().getSeqNum() < ackedNumber){
+			packetWindow.poll();
+		}
+	}
+	
+	private void fillWindow() throws UnknownHostException, IOException{
+		while(packetWindow.size() < currentWindowSize && packetsList.size() != 0){
+			sendSocket.send(new DatagramPacket(moveListHeadToWindow().getPacketData(),
+					  AAPPacket.PACKET_SIZE,InetAddress.getByName(recverAddress), recverPort));
+		}
+	}
+	
+	/**
+	 * 
+	 * @param num The number of packets in window to send
+	 * @throws IOException 
+	 * @throws UnknownHostException 
+	 */
+	
+	private void sendPacketInWindow(int num) throws UnknownHostException, IOException{
+		 for(int i = 0; i < num; i++){
+			 sendSocket.send(new DatagramPacket(packetWindow.get(i).getPacketData(),
+					  AAPPacket.PACKET_SIZE,InetAddress.getByName(recverAddress), recverPort));
+		 }
+		  
 	}
 	
 }
