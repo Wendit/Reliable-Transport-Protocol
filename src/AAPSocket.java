0343a52724786b539be93dcf6b0e0da76003d9c8
Wendi Tang <wtang45@gatech.edu>
2015-11-20 22:33:45 -0500

    Edited

--- src/AAPSocket.java
+++ src/AAPSocket.java
@@ -6,29 +6,36 @@ public class AAPSocket {
 	private String remoteSocketAddress;
 	private int remoteSocketPort;
 	private int localBindPort;
+	private AAPInputStream inputStream;
+	private AAPOutputStream outputStream;
 	
 	/**
 	 * Default constructor
+	 * @throws SocketException 
+	 * @throws UnknownHostException 
 	 */
-	public AAPSocket(String remoteSocketAddress,int remoteSocketPort, int localBindPort){
+	public AAPSocket(String remoteSocketAddress,int remoteSocketPort, int localBindPort) throws UnknownHostException, SocketException{
 		this.remoteSocketAddress = remoteSocketAddress;
 		this.remoteSocketPort = remoteSocketPort;
 		this.localBindPort = localBindPort;
+		this.inputStream = new AAPInputStream(localBindPort,0, remoteSocketAddress, remoteSocketPort);
+		this.outputStream = new AAPOutputStream(localBindPort,0, remoteSocketAddress, remoteSocketPort);
 	}
 	
-	public AAPInputStream getInputStream() throws UnknownHostException, SocketException{
-		return new AAPInputStream(localBindPort,0, remoteSocketAddress, remoteSocketPort);
+	public AAPInputStream getInputStream(){
+		return inputStream;
 	}
 	
 	public AAPOutputStream getOutputStream(){
-		return null;
+		return outputStream;
 	}
 	
 	public void close(){
-		
+		inputStream.close();
+		outputStream.close();
 	}
 	
-	public InetAddress getRemoteSocketAddress(){
-		return null;
+	public InetAddress getRemoteSocketAddress() throws UnknownHostException{
+		return InetAddress.getByName(remoteSocketAddress);
 	}
 }
